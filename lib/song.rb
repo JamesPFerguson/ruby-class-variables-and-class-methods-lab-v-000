0d7708079abd064d949128a9f65da111353455b9
James Ferguson <ncjamesferguson@gmail.com>
2017-12-10 22:26:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  def initialize(name, artists, genre)
+  def initialize(name, artist, genre)
     self.name = name
     self.artist = artist
     self.genre = genre
