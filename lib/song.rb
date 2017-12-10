4f0364768da6f57f9080c68e0dcdcfb23b248571
James Ferguson <ncjamesferguson@gmail.com>
2017-12-10 22:43:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,24 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@artists = []
   @@genres = []
   @@genre_count = {}
   @@artist_count = 0
 
-  attr_accessor :name, :artist, :genre
+
 
   def initialize(name, artist, genre)
 
-    self.name = name
-    self.artist = artist
-    self.genre = genre
-
-    @@artists << artist if !(@@artists.include?(artist))
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
+    
+    @@artists << artist if !(@@artists.include?(artist))
+    
     @@genres << genre if !(@@genres.include?(genre))
 
     if !(@@genre_count.has_key?(genre))
