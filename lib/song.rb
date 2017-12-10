74883add9271152d18c6a050b7a4fa63e8139e6a
James Ferguson <ncjamesferguson@gmail.com>
2017-12-10 22:25:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,16 @@ class Song
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artists, genre)
+    @@artists << name
+    @@songs += 1
+    @@genres << genre
+
+    if !(@@genre_count.has_key?(genre))
+      @@genre_count[genre] = 0
+    end
+
+    @@genre_count[genre] += 1
+
+  end
 
 end
-  
